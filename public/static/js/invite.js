542929335a95fb1aa74ff1650c405a38618b1974
youngkwon02 <youngkwon02@naver.com>
2021-11-24 16:01:58 +0900

    feat: create-room entry arrangement

--- public/static/js/invite.js
+++ public/static/js/invite.js
@@ -20,6 +20,7 @@ window.onload = async () => {
           resultBoard.innerHTML = resultBoard.innerHTML + htmlData;
         }
         for (let i = 0; i < dataList.length; i++) {
+          if (isAlreadyAdd(dataList[i]["kakao_id"])) continue;
           if (dataList[i]["name"] == "") {
             dataList[i]["name"] = "비공개";
           }
@@ -29,7 +30,7 @@ window.onload = async () => {
           if (dataList[i]["email"] == "") {
             dataList[i]["email"] = "비공개";
           }
-          htmlData = `<div class="search-result-row">
+          htmlData = `<div class="search-result-row search-result-row-${dataList[i]["kakao_id"]}">
             <div>이름 : ${dataList[i]["name"]}</div>
             <div>소속 : ${dataList[i]["univ_name"]}</div>
             <div>이메일 : ${dataList[i]["email"]}</div>
@@ -56,6 +57,8 @@ window.onload = async () => {
   } else {
     document.querySelector(".home-entry .entry-univ-span").innerHTML = univName;
     addUserEntry(KAKAOID);
+    document.querySelector(`.entry-body-row-${KAKAOID}`).style.cursor =
+      "auto !important";
   }
 };
 
@@ -79,8 +82,8 @@ const ajaxRequest = (type, url, data) => {
 };
 
 const addUserEntry = async (kakaoId) => {
+  const KAKAOID = document.querySelector(".userKakaoId").innerHTML;
   let ownerUniv = document.querySelector(".userUniv").innerHTML;
-
   let univName = "";
   let userName = "";
   let userEmail = "";
@@ -98,6 +101,13 @@ const addUserEntry = async (kakaoId) => {
     alert("다시 시도하세요.");
     location.href = "/plaza";
   }
+
+  if (KAKAOID !== kakaoId) {
+    // 추가하기 버튼 눌렀을 때, 검색 결과에서 삭제
+    document.querySelector(`.search-result-row-${kakaoId}`).style.display =
+      "none";
+  }
+
   if (univName !== ownerUniv) {
     // Away
     if (univName === null) {
@@ -111,7 +121,7 @@ const addUserEntry = async (kakaoId) => {
       // 용병
       document.querySelector(
         ".away-entry-body"
-      ).innerHTML += `<div class="entry-body-row">[${univName}] ${userName}</div>`;
+      ).innerHTML += `<div class="entry-body-row entry-body-row-${kakaoId}" onclick="popUserEntry(${kakaoId})">[${univName}] ${userName}</div>`;
     } else if (currentAway !== "" && currentAway !== univName) {
       // 상대학교 변경
       modifyAway = confirm("상대 대학을 재설정하시겠습니까?");
@@ -119,7 +129,7 @@ const addUserEntry = async (kakaoId) => {
       // 기존 상대학교 멤버 추가
       document.querySelector(
         ".away-entry-body"
-      ).innerHTML += `<div class="entry-body-row">[${univName}] ${userName}</div>`;
+      ).innerHTML += `<div class="entry-body-row entry-body-row-${kakaoId}" onclick="popUserEntry(${kakaoId})">[${univName}] ${userName}</div>`;
     } else if (currentAway === "") {
       // initial away select
       document.querySelector(
@@ -128,7 +138,7 @@ const addUserEntry = async (kakaoId) => {
       document.querySelector(".away-entry-body").innerHTML = "";
       document.querySelector(
         ".away-entry-body"
-      ).innerHTML += `<div class="entry-body-row">[${univName}] ${userName}</div>`;
+      ).innerHTML += `<div class="entry-body-row entry-body-row-${kakaoId}" onclick="popUserEntry(${kakaoId})">[${univName}] ${userName}</div>`;
     }
     if (modifyAway) {
       document.querySelector(
@@ -137,13 +147,68 @@ const addUserEntry = async (kakaoId) => {
       document.querySelector(".away-entry-body").innerHTML = "";
       document.querySelector(
         ".away-entry-body"
-      ).innerHTML += `<div class="entry-body-row">[${univName}] ${userName}</div>`;
+      ).innerHTML += `<div class="entry-body-row entry-body-row-${kakaoId}" onclick="popUserEntry(${kakaoId})">[${univName}] ${userName}</div>`;
     }
   } else {
     document.querySelector(
       ".home-entry-body"
-    ).innerHTML += `<div class="entry-body-row">[${univName}] ${userName}</div>`;
+    ).innerHTML += `<div class="entry-body-row entry-body-row-${kakaoId}" onclick="popUserEntry(${kakaoId})">[${univName}] ${userName}</div>`;
   }
+};
+
+const popUserEntry = (kakaoId) => {
+  const KAKAOID = document.querySelector(".userKakaoId").innerHTML;
+  if (parseInt(kakaoId) === parseInt(KAKAOID)) {
+    return false;
+  }
+  let className = "entry-body-row-" + kakaoId;
+
+  // Home team check
+  let homeRows = document.querySelectorAll(".home-entry-body .entry-body-row");
+  let htmlContent = "";
+  for (let i = 0; i < homeRows.length; i++) {
+    if (!homeRows[i].className.includes(className)) {
+      htmlContent += homeRows[i].outerHTML;
+    }
+  }
+  document.querySelector(".home-entry-body").innerHTML = htmlContent;
+
+  // Away team check
+  let awayRows = document.querySelectorAll(".away-entry-body .entry-body-row");
+  htmlContent = "";
+  for (let i = 0; i < awayRows.length; i++) {
+    if (!awayRows[i].className.includes(className)) {
+      htmlContent += awayRows[i].outerHTML;
+    }
+  }
+  document.querySelector(".away-entry-body").innerHTML = htmlContent;
+
+  if (htmlContent === "") {
+    document.querySelector(".away-entry .entry-univ-span").innerHTML = "";
+  }
+
+  document.querySelector(".user-search").value = "";
+  document.querySelector(".user-search-result").innerHTML = "";
+};
+
+const isAlreadyAdd = (kakaoId) => {
+  let className = "entry-body-row-" + kakaoId;
+  // Home team check
+  let homeRows = document.querySelectorAll(".home-entry-body .entry-body-row");
+  let htmlContent = "";
+  for (let i = 0; i < homeRows.length; i++) {
+    if (homeRows[i].className.includes(className)) {
+      return true;
+    }
+  }
+  // Away team check
+  let awayRows = document.querySelectorAll(".away-entry-body .entry-body-row");
+  for (let i = 0; i < awayRows.length; i++) {
+    if (awayRows[i].className.includes(className)) {
+      return true;
+    }
+  }
+  return false;
 };
 
 function KeyCheck(event) {
